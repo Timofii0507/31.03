47dfb7fe1a094f542183d52c29ee4cecfce5b41c
Юрков Тимофій Олександрович <145855201+Timofii0507@users.noreply.github.com>
2024-04-09 17:51:34 +0300

    Друге  завдання  з  дз

--- 31.03/MainWindow.xaml.cs
+++ 31.03/MainWindow.xaml.cs
@@ -9,31 +9,171 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static MaterialDesignThemes.Wpf.Theme;
 
 namespace _31._03
 {
     public partial class MainWindow : Window
     {
+        private double lastNumber, result;
+        private SelectedOperator selectedOperator;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            acButton.Click += AcButton_Click;
+            negativeButton.Click += NegativeButton_Click;
+            percentageButton.Click += PercentageButton_Click;
+            equalsButton.Click += EqualsButton_Click;
+        }
+
+        private void EqualsButton_Click(object sender, RoutedEventArgs e)
+        {
+            double newNumber;
+            if (double.TryParse(resultLabel.Text.ToString(), out newNumber))
+            {
+                switch (selectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        result = SimpleMath.Add(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Subtraction:
+                        result = SimpleMath.Subtract(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result = SimpleMath.Multiply(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Division:
+                        result = SimpleMath.Divide(lastNumber, newNumber);
+                        break;
+                }
+
+                resultLabel.Text = result.ToString();
+            }
+        }
+
+        private void PercentageButton_Click(object sender, RoutedEventArgs e)
+        {
+            double tempNumber;
+            if (double.TryParse(resultLabel.Text.ToString(), out tempNumber))
+            {
+                tempNumber = tempNumber / 100;
+                if (lastNumber != 0)
+                    tempNumber *= lastNumber;
+                resultLabel.Text = tempNumber.ToString();
+            }
+        }
+
+        private void NegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Text.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                resultLabel.Text = lastNumber.ToString();
+            }
+        }
+
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Text = "0";
+        }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Text.ToString(), out lastNumber))
+            {
+                resultLabel.Text = "0";
+            }
+
+            if (sender == multiplyButton)
+                selectedOperator = SelectedOperator.Multiplication;
+            if (sender == divideButton)
+                selectedOperator = SelectedOperator.Division;
+            if (sender == plusButton)
+                selectedOperator = SelectedOperator.Addition;
+            if (sender == minusButton)
+                selectedOperator = SelectedOperator.Subtraction;
+        }
+
+        private void PointButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!resultLabel.Text.ToString().Contains("."))
+            {
+                resultLabel.Text = $"{resultLabel.Text}.";
+            }
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedValue = 0;
+
+            if (sender == zeroButton)
+                selectedValue = 0;
+            if (sender == oneButton)
+                selectedValue = 1;
+            if (sender == twoButton)
+                selectedValue = 2;
+            if (sender == threeButton)
+                selectedValue = 3;
+            if (sender == fourButton)
+                selectedValue = 4;
+            if (sender == fiveButton)
+                selectedValue = 5;
+            if (sender == sixButton)
+                selectedValue = 6;
+            if (sender == sevenButton)
+                selectedValue = 7;
+            if (sender == eightButton)
+                selectedValue = 8;
+            if (sender == nineButton)
+                selectedValue = 9;
+
+            if (resultLabel.Text.ToString() == "0")
+            {
+                resultLabel.Text = $"{selectedValue}";
+            }
+            else
+            {
+                resultLabel.Text = $"{resultLabel.Text}{selectedValue}";
+            }
         }
     }
 
-    public class WidthConverter : IValueConverter
+    public enum SelectedOperator
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+
+    public class SimpleMath
+    {
+        public static double Add(double n1, double n2)
         {
-            if (value is double width)
-            {
-                return width * 2 / 3; 
-            }
-            return value;
+            return n1 + n2;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public static double Subtract(double n1, double n2)
         {
-            throw new NotImplementedException();
+            return n1 - n2;
+        }
+
+        public static double Multiply(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public static double Divide(double n1, double n2)
+        {
+            if (n2 == 0)
+            {
+                MessageBox.Show("Division by 0 is not supported.", "Wrong operation", MessageBoxButton.OK, MessageBoxImage.Error);
+                return 0;
+            }
+
+            return n1 / n2;
         }
     }
 }
